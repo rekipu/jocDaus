72b17ced4a6e65906f06da1714a274e1f71043d3
Joan Manel Ardura Terra <jarduraterra@gmail.com>
2019-08-01 12:50:13 +0200

    creat projecte spring

---
+++ exerciciDaus/src/main/java/io/reki/ExerciciDausApplication.java
@@ -0,0 +1,13 @@
+package io.reki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciDausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciDausApplication.class, args);
+	}
+
+}

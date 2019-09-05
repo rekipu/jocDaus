2c2403f001152a18a4a4a915fe16d64a821ffc09
Joan Manel Ardura Terra <jarduraterra@gmail.com>
2019-09-05 12:23:18 +0200

    bastant funcional, falta afegir consultes

--- exerciciDaus/src/main/java/io/reki/ExerciciDausApplication.java
+++ exerciciDaus/src/main/java/io/reki/ExerciciDausApplication.java
@@ -7,13 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExerciciDausApplication {
+	public static void neteja() {
+		for (int i = 0; i < 30; ++i) System.out.println();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExerciciDausApplication.class, args);
-		
+
 		Scanner lee = new Scanner(System.in);
 		int select;
 		int playerId = 0;
+
 		do {
 			System.out.println("Benvingut al dad'o'matic!");
 			System.out.println("Per registrar un jugador nou, escriu 1");
@@ -23,37 +27,44 @@ public class ExerciciDausApplication {
 			select = lee.nextInt();
 			switch (select) {
 			case 1:
+				neteja();
 				System.out.println("Escriu el nom de jugador, o deixa en blanc per logejar anonimament: ");
 				Player player = new Player(lee.next());
-				System.out.println("Benvingut, "+ player.getPlayerName() + " , el teu id es: " + player.getId());
+				System.out.println("Benvingut, " + player.getPlayerName() + " , el teu id es: " + player.getId());
 				playerId = player.getId();
 				break;
 			case 2:
-				if (playerId == 0){
+				neteja();
+				if (playerId == 0) {
 					System.out.println("Has d'estar logejat com a jugador per tirar daus");
 					break;
-				}else {
-					String resposta;			
+				} else {
+					String resposta;
 					do {
-					ThrowDices tirada = new ThrowDices(playerId);
-					System.out.println("Vols tornar a tirar? S/N");
-					resposta = lee.next().toUpperCase();
-					}while(resposta == "S");
+						neteja();
+						ThrowDices tirada = new ThrowDices(playerId);
+						System.out.println("Vols tornar a tirar? S/N");
+						resposta = lee.next();
+					} while (resposta.toUpperCase().equals("S"));
 					break;
 				}
 			case 3:
-				//TODO consultes
+				neteja();
+				// TODO consultes
 				System.out.println("blablabla historial");
+				break;
+			default:
+				break;	
 			}
-				
-				
 			
-			
-			
-		}while(select==1||select==2||select==3);
+
+		} while (select == 1 || select == 2 || select == 3);
 		
-		/*ThrowDices tirada = new ThrowDices(3);
-		tirada.tira();*/
+		System.out.println("Bye bye, hasta otro ratito!");
+
+		/*
+		 * ThrowDices tirada = new ThrowDices(3); tirada.tira();
+		 */
 	}
 
 }
